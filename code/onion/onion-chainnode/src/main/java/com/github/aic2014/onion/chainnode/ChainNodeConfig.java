7b3a3ab7bac76f4e0410447d92e5ec7e3417a5a7
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2014-10-23 23:00:34 +0200

    added a bean to chain node that retrieves the server port upon startup

--- code/onion/onion-chainnode/src/main/java/com/github/aic2014/onion/chainnode/ChainNodeConfig.java
+++ code/onion/onion-chainnode/src/main/java/com/github/aic2014/onion/chainnode/ChainNodeConfig.java
@@ -1,6 +1,12 @@
 package com.github.aic2014.onion.chainnode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -11,6 +17,30 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("file:${ONION_CONF_DIR}/chainnode.properties")
 public class ChainNodeConfig
 {
-  //TODO: add bean that registers with the directory node upon startup
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+  /**
+   * Bean that registers an application listener. When the application container starts up
+   * the listener is called and retrieves the host/port from the container and
+   * registers with the directory node.
+   *
+   * @return
+   */
+  @Bean
+  public ApplicationListener<EmbeddedServletContainerInitializedEvent> getPortDiscoveryBean() {
+    ApplicationListener<EmbeddedServletContainerInitializedEvent> listener = new
+      ApplicationListener<EmbeddedServletContainerInitializedEvent>()
+      {
+        @Override
+        public void onApplicationEvent(final EmbeddedServletContainerInitializedEvent embeddedServletContainerInitializedEvent) {
+          EmbeddedServletContainer container = embeddedServletContainerInitializedEvent.getEmbeddedServletContainer();
+          int port = container.getPort();
+          logger.debug("servlet container initialized, port is {}", port);
+
+        }
+      };
+    return listener;
+  }
+
+
 
 }
