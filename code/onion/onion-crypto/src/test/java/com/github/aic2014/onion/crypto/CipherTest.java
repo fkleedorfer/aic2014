52b7ff1c7057793994b522cd387dbf616a5e7dd5
Daniel Achleitner <daniel.achleitner@gmail.com>
2014-11-12 23:58:27 +0100

    slf4j logging in tests

--- code/onion/onion-crypto/src/test/java/com/github/aic2014/onion/crypto/CipherTest.java
+++ code/onion/onion-crypto/src/test/java/com/github/aic2014/onion/crypto/CipherTest.java
@@ -2,6 +2,8 @@ package com.github.aic2014.onion.crypto;
 
 import com.github.aic2014.onion.crypto.cipher.*;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
@@ -13,13 +15,14 @@ import java.security.KeyPair;
 
 public class CipherTest {
     public static String vulcan = "Eyjafjallaj\u00f6kull";
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Test
     public void RSACryptorTest() throws GeneralSecurityException {
         KeyPair kp = Generators.generateRSAKeyPair();
         Cipher<String, String> rsa = new StringCipherAdapter<>(new Base64CipherAdapter<>(new RSACipher(kp)));
         String e = rsa.encrypt(vulcan);
-        System.out.printf("rsa(%s): %s%n", vulcan, e);
+        logger.info("rsa({}): {}", vulcan, e);
         String d = rsa.decrypt(e);
         assertEquals(vulcan, d);
     }
@@ -30,7 +33,7 @@ public class CipherTest {
         IvParameterSpec iv = Generators.generateIV();
         Cipher<String, String> aes = new StringCipherAdapter<>(new Base64CipherAdapter<>(new AESCipher(key, iv)));
         String e = aes.encrypt(vulcan);
-        System.out.printf("aes(%s): %s%n", vulcan, e);
+        logger.info("aes({}): {}", vulcan, e);
         String d = aes.decrypt(e);
         assertEquals(vulcan, d);
     }
