61080d5c11b89b47ed42c702bb60f9586e69624f
Fabian Theuretzbacher <fabthe@gmail.com>
2014-12-04 05:09:30 +0100

    chain node deployment script, which is called *locally* on the directory node to install a chainnode

---
+++ code/onion/deployment/chainnode-deployment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+REMOTEHOST=$1
+IDFILE="/D/Drive/TU Wien/2014WS-Advanced Internet Computing/SSH/G6-T3-id_rsa.pem"
+# IDFILE="/home/onion/directorynode/deployment/G6-T3-id_rsa.pem"
+SOURCEDIR='../deployment/chainnode/*'
+# SOURCEDIR='/home/onion/directorynode/deployment/chainnode/*'
+REMOTEDIR='~/chainnode/'
+
+echo "Upload files..."
+scp -oStrictHostKeyChecking=no -i "${IDFILE}" -r ${SOURCEDIR} onion@${REMOTEHOST}:${REMOTEDIR}
+
+echo "Start Application..."
+ssh -oStrictHostKeyChecking=no -i "${IDFILE}" onion@${REMOTEHOST} "nohup java \
+     -DONION_CONF_DIR=/home/onion/chainnode/conf.local \
+     -Dlogging.config=/home/onion/chainnode/conf.local/logback.xml \
+     -jar /home/onion/chainnode/onion-chainnode-1.0-SNAPSHOT-allinone.jar >/dev/null 2>&1 &"
+
+echo "Done!"
