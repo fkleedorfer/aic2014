0171058f512221635ca3cee353c47ec3dbc0429e
Daniel Achleitner <daniel.achleitner@gmail.com>
2015-01-19 20:56:43 +0100

    Resend/retry lost request messages #30

--- code/onion/onion-client/src/main/java/com/github/aic2014/onion/client/OnionClientCommandLineRunner.java
+++ code/onion/onion-client/src/main/java/com/github/aic2014/onion/client/OnionClientCommandLineRunner.java
@@ -1,6 +1,5 @@
 package com.github.aic2014.onion.client;
 
-import com.github.aic2014.onion.model.ChainNodeInfo;
 import com.github.aic2014.onion.shell.Command;
 import com.github.aic2014.onion.shell.Shell;
 import org.apache.http.HttpRequest;
@@ -15,95 +14,83 @@ import org.springframework.boot.CommandLineRunner;
 
 import java.io.ByteArrayOutputStream;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 
-public class OnionClientCommandLineRunner implements CommandLineRunner
-{
+public class OnionClientCommandLineRunner implements CommandLineRunner {
 
-  private Shell shell;
-  private static ExecutorService executor;
+    private Shell shell;
+    private static ExecutorService executor;
 
-  @Autowired
-  private OnionClient client;
+    @Autowired
+    private OnionClient client;
 
-  @Value("${quoteserver.baseUri}")
-  private String quoteServerUri;
+    @Value("${quoteserver.baseUri}")
+    private String quoteServerUri;
 
-  @Value("${quoteserver.hostnamePort}")
-  private String quoteServerHostnamePort;
+    @Value("${quoteserver.hostnamePort}")
+    private String quoteServerHostnamePort;
 
-  @Override
-  public void run(final String... strings) throws Exception {
-      shell = new Shell("Client", System.in, System.out);
-      shell.register(this);
-      executor = Executors.newFixedThreadPool(20);
-      executor.execute(shell);
-      printUsage();
-  }
+    @Override
+    public void run(final String... strings) throws Exception {
+        shell = new Shell("Client", System.in, System.out);
+        shell.register(this);
+        executor = Executors.newFixedThreadPool(20);
+        executor.execute(shell);
+        printUsage();
+    }
 
-  @Command(value = "help")
-  public void printUsage() throws Exception {
-      shell.writeLine("Welcome to the Onion Routing Demo! :)");
-      shell.writeLine("This are your commands:");
-      shell.writeLine("!send ... sends a request and prints the response to the Console");
-      shell.writeLine("!bomb N ... sends N requests multiple parallel threads");
-      shell.writeLine("!help ... shows this usage notice");
-      shell.writeLine("!exit ... stops the Client");
-  }
+    @Command(value = "help")
+    public void printUsage() throws Exception {
+        shell.writeLine("Welcome to the Onion Routing Demo! :)");
+        shell.writeLine("This are your commands:");
+        shell.writeLine("!send ... sends a request and prints the response to the Console");
+        shell.writeLine("!bomb N ... sends N requests multiple parallel threads");
+        shell.writeLine("!help ... shows this usage notice");
+        shell.writeLine("!exit ... stops the Client");
+    }
 
-  @Command
-  public String send() throws Exception {
+    @Command
+    public String send() throws Exception {
 
-      String requestString = buildRequestString();
-      shell.writeLine("Sending Request: " + requestString);
+        String requestString = buildRequestString();
+        shell.writeLine("Sending Request: " + requestString);
 
-      ChainNodeInfo[] chain = client.getChain();
-      shell.writeLine("-------------------------------");
+        OnionRoutedHttpRequest request = client.getHttpRequest();
+        String response = request.execute(requestString);
 
-      shell.writeLine("Used Chain:");
-      for(int i = 0; i < 3; i++) {
-          shell.writeLine("ChainNode"+i+": " + chain[i].getPublicIP() + ":" + chain[i].getPort());
-      }
-      shell.writeLine("-------------------------------");
+        shell.writeLine(request.printUsedChain());
+        return String.format("Response in %s ms: %s", request.getRoundTripTime(), response);
+    }
 
-      long start = System.nanoTime();
-      // print
-      String response = client.executeOnionRoutedHttpRequest(requestString, chain);
-
-      return String.format("Response in %s msec: %s", (System.nanoTime() - start) / 1000 / 1000, response);
-  }
-
-  @Command
-  public String bomb(int messageCount) throws Exception {
-      final String requestString = buildRequestString();
-      final CountDownLatch latch = new CountDownLatch(messageCount);
-      Runnable sendTask = new Runnable(){
-          @Override
-          public void run() {
-              try {
-                  ChainNodeInfo[] chain = client.getChain();
-                  String response = client.executeOnionRoutedHttpRequest(requestString, chain);
-              } catch (Exception e) {
-                  e.printStackTrace();
-              } finally {
-                  latch.countDown();
-              }
-          }
-      };
-      shell.writeLine("-------------------------------");
-      shell.writeLine(String.format("Sending %s messages...", messageCount));
-      for (int i = 0; i < messageCount; i++) {
-          executor.execute(sendTask);
-      }
-      latch.await();
-      return "Received all responses";
-  }
+    @Command
+    public String bomb(int messageCount) throws Exception {
+        final String requestString = buildRequestString();
+        final CountDownLatch latch = new CountDownLatch(messageCount);
+        Runnable sendTask = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OnionRoutedHttpRequest request = client.getHttpRequest();
+                    request.execute(requestString);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            }
+        };
+        shell.writeLine("-------------------------------");
+        shell.writeLine(String.format("Sending %s messages...", messageCount));
+        for (int i = 0; i < messageCount; i++) {
+            executor.execute(sendTask);
+        }
+        latch.await();
+        return "Received all responses";
+    }
 
     private String buildRequestString() throws java.io.IOException, org.apache.http.HttpException {
-        HttpGet request = new HttpGet(quoteServerUri+"/quote");
+        HttpGet request = new HttpGet(quoteServerUri + "/quote");
         request.addHeader("Host", quoteServerHostnamePort);
         HttpTransportMetricsImpl metrics = new HttpTransportMetricsImpl();
         SessionOutputBufferImpl sessionOutputBuffer = new SessionOutputBufferImpl(metrics, 255);
@@ -117,11 +104,11 @@ public class OnionClientCommandLineRunner implements CommandLineRunner
 
 
     @Command
-  public String exit() throws Exception {
-      shell.writeLine("Stopping the client ...");
-      shell.writeLine("Client has been stopped!");
-      executor.shutdownNow();
-      System.exit(0);
-      return "";
-  }
+    public String exit() throws Exception {
+        shell.writeLine("Stopping the client ...");
+        shell.writeLine("Client has been stopped!");
+        executor.shutdownNow();
+        System.exit(0);
+        return "";
+    }
 }
---
+++ code/onion/onion-client/src/main/java/com/github/aic2014/onion/client/OnionRoutedHttpRequest.java
@@ -0,0 +1,112 @@
+package com.github.aic2014.onion.client;
+
+import com.github.aic2014.onion.crypto.CryptoService;
+import com.github.aic2014.onion.json.JsonUtils;
+import com.github.aic2014.onion.model.ChainNodeInfo;
+import com.github.aic2014.onion.model.Message;
+import com.github.aic2014.onion.model.OnionStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClientException;
+
+import java.net.URI;
+import java.util.UUID;
+
+public class OnionRoutedHttpRequest extends OnionRoutedRequest {
+
+    public OnionRoutedHttpRequest(CryptoService cryptoService, URI directoryNodeURI, int chainErrorRetries) {
+        super(cryptoService, directoryNodeURI, chainErrorRetries);
+    }
+
+    public String execute(String request) throws OnionRoutedRequestException {
+        String response = "";
+
+        for (int retry = 1; retry <= chainErrorRetries || chainErrorRetries < 1; retry++) {
+            try {
+
+                response = executeRequest(request);
+                break;
+
+            } catch (OnionRoutedRequestException e) {
+
+                // chain is defunct: fetch a new chain and retry
+                if (e.getStatus() == OnionStatus.CHAIN_ERROR || e.getStatus() == OnionStatus.CHAIN_TIMEOUT) {
+                    logger.info("Chain error {}: {}", e.getStatus(), e.getFailedNode());
+                    logger.info("Retrying request ({}/{})...", retry, chainErrorRetries);
+                    continue;
+                }
+
+                throw e;
+            }
+        }
+
+        return response;
+    }
+
+    private String executeRequest(String request) throws OnionRoutedRequestException {
+
+        fetchNewChain();
+        Message msg = buildMessage(usedChain, usedChainID, request);
+        logger.debug("sending this message: {}", msg);
+
+        try {
+            long start = System.nanoTime();
+            ResponseEntity<Message> responseEntity = restTemplate.postForEntity(usedChain[0].getUri().toString() + "/request", msg, Message.class);
+            Message responseMessage = responseEntity.getBody();
+            String responseString = decryptResponse(responseMessage);
+            roundTripTime = (System.nanoTime() - start) / 1000 / 1000;
+            return responseString;
+
+        } catch (RestClientException e) {
+            OnionRoutedRequestException o = new OnionRoutedRequestException(OnionStatus.CHAIN_ERROR, e);
+            o.setFailedNode(usedChain[0]);
+            throw o;
+        }
+    }
+
+    private Message buildMessage(final ChainNodeInfo[] chain, UUID chainId, final String payload) {
+        logger.debug("building message for chain {} and payload {}", chain, payload);
+
+        String lastPayload = payload;
+        Message msg = null;
+        //from last call to first call, create message object, serialize it and
+        //add it encrypted as payload to the one before.
+        //start by building the outermost
+        for (int idx = chain.length - 1; idx >= 0; idx--) {
+            msg = new Message();
+            msg.setChainId(chainId);
+            msg.setRecipient(chain[idx].getUri());
+            if (idx > 0) {
+                msg.setSender(chain[idx - 1].getUri());
+            }
+            msg.setHopsToGo(chain.length - 1 - idx);
+            msg.setPublicKey(this.cryptoService.getPublicKey());
+            msg.setPayload(this.cryptoService.encrypt(lastPayload, chain[idx].getPublicKey()));
+            logger.debug("message for chain step {}: {}", idx, msg);
+            //now, convert the newly built message to a payload for the next message
+            lastPayload = JsonUtils.toJSON(msg);
+        }
+        return msg;
+    }
+
+    private String decryptResponse(Message msg) throws OnionRoutedRequestException {
+        int i;
+
+        // decrypt payload, starting from first chain node to last
+        for (i = 1; i < usedChain.length; i++) {
+            if (msg.getStatus() != OnionStatus.OK) {
+                break;
+            }
+            String payload = this.cryptoService.decrypt(msg.getPayload());
+            msg = JsonUtils.fromJSON(payload);
+        }
+
+        if (msg.getStatus() == OnionStatus.OK) {
+            return this.cryptoService.decrypt(msg.getPayload());
+        }
+
+        OnionRoutedRequestException e = new OnionRoutedRequestException(msg.getStatus());
+        e.setFailedNode(usedChain[i - 1]);
+        throw e;
+    }
+}
